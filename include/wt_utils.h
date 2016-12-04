a82b519aeb1d341bba8fb4a7cb7d05a817029f7b
spastorelli <steeve.pastorelli@gmail.com>
2016-12-04 22:24:20 +0000

    Add impl to read WT cell data.

---
+++ include/wt_utils.h
@@ -0,0 +1,16 @@
+#ifndef WT_UTILS_H_
+#define WT_UTILS_H_
+
+#include <stdint.h>
+
+#define POS_1BYTE_MARKER (uint8_t)0x80
+#define POS_2BYTE_MARKER (uint8_t)0xc0
+#define POS_MULTI_MARKER (uint8_t)0xe0
+
+#define POS_1BYTE_MAX ((1 << 6) - 1)
+#define POS_2BYTE_MAX ((1 << 13) + POS_1BYTE_MAX)
+
+
+int unpackUInt(const uint8_t **p_data, uint64_t *xp);
+
+#endif      // WT_UTILS_H_
--- wt_file_block.cpp
+++ wt_file_block.cpp
@@ -3,19 +3,71 @@
 #include <iostream>
 
 #include "include/wt_file.h"
+#include "include/wt_utils.h"
 
 WTBlockReader::WTBlockReader(struct wt_file_reader *reader) {
   wt_r = reader;
   assert(wt_r != NULL);
 };
 
-uint8_t* WTBlockReader::readBlock(int offset, int length) {
-  uint8_t *data = new uint8_t[length];
+int WTBlockReader::readCell(wt_cell_unpack_t *unpack_c, int offset) {
+  /* Get start of the cell from the mapped memory region */
+  uint8_t* cell = static_cast<uint8_t *>(wt_r->map) + offset;
 
-  memcpy(data, (static_cast<uint8_t*>(wt_r->map) + offset), length);
+  unpack_c->prefix = 0;
+  unpack_c->data = NULL;
+  unpack_c->size = 0;
+  unpack_c->len = 0;
 
-  uint32_t desc = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
-  std::cout << "desc: " << std::dec << desc << std::endl;
+  wt_cell_header_t c_head;
+  std::memcpy(&c_head, cell, sizeof(wt_cell_header_t));
+  unpack_c->desc = c_head.__chunk[0];
 
-  return data;
+  /* Short cell type */
+  switch (unpack_c->desc & WT_CELL_SHORT_TYPE_MASK) {
+    case WT_CELL_KEY_SHORT:
+    case WT_CELL_VALUE_SHORT:
+      unpack_c->size = (unpack_c->desc >> WT_CELL_SHORT_SHIFT);
+      unpack_c->data = new uint8_t[unpack_c->size];
+      std::memcpy(unpack_c->data, (c_head.__chunk + 1), unpack_c->size);
+      unpack_c->len = 1 + unpack_c->size; /* desc + data */
+      return (unpack_c->len);
+    case WT_CELL_KEY_SHORT_PFX:
+      unpack_c->prefix = c_head.__chunk[1];
+      unpack_c->size = (unpack_c->desc >> WT_CELL_SHORT_SHIFT);
+      unpack_c->data = new uint8_t[unpack_c->size];
+      std::memcpy(unpack_c->data, (c_head.__chunk + 2), unpack_c->size);
+      unpack_c->len = 2 + unpack_c->size; /* desc + prefix + data */
+      return (unpack_c->len);
+  }
+
+  /* Long cell type */
+
+  /* Advance to the cell data */
+  const uint8_t* cell_data = cell + 1;
+  uint64_t sz;
+
+  switch (unpack_c->desc & WT_CELL_LONG_TYPE_MASK) {
+    /* TODO: Handle other cell types */
+    case WT_CELL_ADDR_DEL:
+    case WT_CELL_ADDR_INT:
+    case WT_CELL_ADDR_LEAF:
+    case WT_CELL_ADDR_LEAF_NO:
+    case WT_CELL_KEY:
+    case WT_CELL_KEY_PFX:
+    case WT_CELL_VALUE:
+      unpackUInt(&cell_data, &sz);
+      if (unpack_c->desc == WT_CELL_KEY ||
+          unpack_c->desc == WT_CELL_KEY_PFX ||
+          (unpack_c->desc == WT_CELL_VALUE && !(unpack_c->desc & WT_CELL_64V)))
+          sz += WT_CELL_SIZE_ADJUST;
+
+      unpack_c->size = (uint32_t)sz;
+      unpack_c->data = new uint8_t[unpack_c->size];
+      std::memcpy(unpack_c->data, cell_data, unpack_c->size);
+      unpack_c->len = (cell_data + unpack_c->size) - cell;
+      return (unpack_c->len);
+  }
+  /* TODO: Handle unrecognized used case better */
+  return -1;
 }
